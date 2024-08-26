a60bf792147cf093d8a2d74a150e27b5fe382199
Hassaan Arain <rafatnaz21@gmail.com>
2024-08-26 14:25:47 +0500

    first commit

--- components/homeconponents/home.tsx
+++ components/homeconponents/home.tsx
@@ -6,7 +6,7 @@ export default function Homepage() {
     <div>
       <div className="text-white bg-customBlack pt-32 pl-[10%]">
         <h2>Frontend Developer</h2>
-        <h1 className="text-5xl py-3">Hello I'm</h1>
+        <h1 className="text-5xl py-3">Hello I am</h1>
         <h1 className="text-customBlue text-5xl py-3">Hassaan Arain</h1>
         <p className="text-gray-400">
           A passoniate Web developer from Hyderabad Pakistan.
@@ -14,8 +14,8 @@ export default function Homepage() {
           Intelligence web 3.0 and Metaverce Advanced Courses.
         </p>
 
-        <div className="flex py-14">
-          <button className="rounded-3xl p-2 border border-customBlue mr-[12%] hover:bg-customBlue hover:text-customBlack">
+        <div className="flex py-10">
+          <button className=" w-24  rounded-3xl p-2 border border-customBlue mr-[12%] hover:bg-customBlue hover:text-customBlack">
             Contact
           </button>
           <nav className="p-2  flex text-customBlue">
@@ -45,8 +45,43 @@ export default function Homepage() {
             </Link>
           </nav>
         </div>
+        <div className="flex justify-center py-10">
+          <div className="space-y-5 sm:flex sm:justify-between sm:w-screen sm:space-y-0 sm:px-3 sm:pr-40">
+            <div className="flex">
+              <h1 className="text-6xl sm:text-3xl pr-4 sm:pr-3 ">08</h1>
+              <h1 className="text-xl sm:text-xs">
+                Months of
+                <br />
+                Experience
+              </h1>
+            </div>
+            <div className="flex">
+              <h1 className="text-6xl sm:text-3xl pr-4 sm:pr-3 ">15</h1>
+              <h1 className="text-xl sm:text-xs">
+                Projects
+                <br />
+                Completed
+              </h1>
+            </div>
+            <div className="flex">
+              <h1 className="text-6xl sm:text-3xl pr-4 sm:pr-3 ">09</h1>
+              <h1 className="text-xl sm:text-xs">
+                Technology
+                <br />
+                Mastered
+              </h1>
+            </div>
+            <div className="flex">
+              <h1 className="text-6xl sm:text-3xl pr-4 sm:pr-3 ">120</h1>
+              <h1 className="text-xl sm:text-xs">
+                code
+                <br /> commits
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>right</div>
+      {/* <div>right</div> */}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Navbar from "../../components/navbar";
 import Homepage from "../../components/homeconponents/home";
 export default function Home() {
   return (
     <div>
-    <Homepage/>
+    
+     <Homepage />
+   
     </div>
   );
 }
